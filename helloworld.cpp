1499f7a1f2b0dbe12b5b7164ea29c625615314e2
David Martin <david.martin@in.tum.de>
2022-07-10 13:38:50 +0200

    pipeline test

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!";
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Does the CI/CD Pipeline work?" << std::endl;
   return 0;
 }
